8d91fb2578c1c1d908d388ad8bdaa866195f0fd4
kojisato118 <k.s.9190118@gmail.com>
2016-02-13 18:41:23 +0900

    show projects list in root, but getting data from model not from api

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-    @images = []
-    for i in 0..10
-      @images.append("https://avatars3.githubusercontent.com/u/4058115?v=3&s=460")
-    end
+    @projects = Project.find_projects
   end
 
   def show
