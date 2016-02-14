2f308ba1db35b6bb3ba8e1b2908703149415a6c5
kojisato118 <k.s.9190118@gmail.com>
2016-02-14 19:16:12 +0900

    fix swagger app_url whether production or not

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,4 +1,9 @@
 # https://github.com/ruby-grape/grape-swagger-rails
 # Readmeには/swagger.josnとあるが、.jsonをつけるとapi自体のjsonが読み込まれなかった
 GrapeSwaggerRails.options.url      = '/api/swagger_doc'
-GrapeSwaggerRails.options.app_url  = 'http://localhost:3000'
+
+if Rails.env == 'production'
+  GrapeSwaggerRails.options.app_url  = 'http://sprint-koji.herokuapp.com'
+else
+  GrapeSwaggerRails.options.app_url  = 'http://localhost:3000'
+end
