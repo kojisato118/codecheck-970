980a670d6bf97df5ac9988e2fbdab73ecd498e90
kojisato118 <k.s.9190118@gmail.com>
2016-02-15 18:22:28 +0900

    fix post project page. use api directory in form tag not throuph controller.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,16 +9,4 @@ class ProjectsController < ApplicationController
   def new
     # TODO: 何かしら認証かける
   end
-
-  def create
-    # TODO: ここだけじゃなくurlとか見たいなlocalhostとproductionとかでわけたい定数の設定
-    con = Faraday::Connection.new(:url => 'http://sprint-koji.herokuapp.com/')
-    response = con.post '/api/projects', params
-    # TODO: エラーや成功についてアラートする
-    if response.status == 201
-      redirect_to "/"
-    else
-      redirect_to "/projects/new"
-    end
-  end
 end
