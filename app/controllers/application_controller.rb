7113655344e9e13bf8b86bac602763071bad4314
kojisato118 <k.s.9190118@gmail.com>
2016-02-21 12:12:34 +0900

    add links to header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,4 @@ class ApplicationController < ActionController::Base
     def sign_in_required
       redirect_to new_user_session_url unless user_signed_in?
     end
-
-  def current_user
-    devise_current_user
-  end
 end
